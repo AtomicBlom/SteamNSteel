2827f3d09eb65e26d494cfac550d1fa22d703a81
Steven Blom <atomicblom@binaryvibrance.net>
2015-04-08 22:47:00 +0930

    Rorax made it unnecessary to do additional translations and rotations

--- src/main/java/mod/steamnsteel/client/renderer/tileentity/SpiderFactoryTESR.java
+++ src/main/java/mod/steamnsteel/client/renderer/tileentity/SpiderFactoryTESR.java
@@ -28,16 +28,11 @@ public class SpiderFactoryTESR extends SteamNSteelTESR
         if (textureId != glTextureId) {
             GL11.glBindTexture(GL11.GL_TEXTURE_2D, glTextureId);
         }
-        //bindTexture(TEXTURE);
 
         GL11.glPushMatrix();
         GL11.glColor4f(1F, 1F, 1F, 1F);
         GL11.glTranslated(x1, y1, z1);
-        GL11.glTranslated(0.5, 0, 0.5);
         GL11.glRotatef(orientation * 90f, 0f, 1f, 0f);
-        GL11.glTranslated(0, 0, .700);
-        GL11.glRotatef(180, 0f, 1f, 0f);
-        //GL11.glRotatef(180F, 1F, 0F, 0F);
 
         ModelManager.INSTANCE.spiderFactoryModel.render();
         GL11.glPopMatrix();
